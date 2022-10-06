062476ddcd6b0c343a24b01fdd06829385185bd0
mllrdev-kennrhey-miraflores <kennrhey.miraflores@mllrdev.com>
2022-10-06 17:43:40 +0800

    refactor: change to object oriented

--- name_sorter.py
+++ name_sorter.py
@@ -3,38 +3,141 @@ import csv
 input_file_name = "random_name.csv"
 output_file_name = "sorted_name.csv"
 
-# initializing the titles and rows list
-fields = []
-rows = []
+class CSVOperations:
+    def __init__(self, input_file, output_file):
+        self.field_names = []
+        self.rows = []
+        self.input_file = input_file
+        self.output_file = output_file
 
-# reading csv file
-with open(input_file_name, 'r') as csvfile:
-    # creating a csv reader object
-    csvreader = csv.reader(csvfile)
-     
-    # extracting field names through first row
-    fields = next(csvreader)
- 
-    # extracting each data row one by one
-    for row in csvreader:
-        rows.append(row)
+    def set_rows_data_from_csv(self):
+        # read input csv file
+        with open(self.input_file, 'r') as csvfile:
+        # creating a csv reader object
+            csvreader = csv.reader(csvfile)
+        
+            # extracting field names through first row
+            self.field_names = next(csvreader)
+        
+            # extracting each data row one by one
+            for row in csvreader:
+                self.rows.append(row)
+
+    def create_output_csv(self, field_names, row_data):
+        #write output csv file
+        with open(self.output_file, 'w', encoding='UTF8', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+
+            # write the header
+            writer.writerow(field_names)
+
+            # write the data
+            writer.writerows(row_data)
+
+    def get_rows(self):
+        return self.rows
+
+    def get_field_names(self):
+        return self.field_names
+
 
 #takes the second element of array
 def take_second(elem):
     return elem[1] 
 
-#sort by first name then last name
-rows.sort()
-rows.sort(key=take_second)
+def sort_names(rows = list):
+    rows.sort() 
+    rows.sort(key=take_second)
+    return rows
 
 
-with open(output_file_name, 'w', encoding='UTF8', newline='') as csvfile:
-    writer = csv.writer(csvfile)
+csv_process = CSVOperations(input_file_name, output_file_name)
+csv_process.set_rows_data_from_csv()
 
-    # write the header
-    writer.writerow(fields)
+unsorted_rows = csv_process.get_rows()
+field_names = csv_process.get_field_names()
 
-    # write the data
-    writer.writerows(rows)
+sorted_rows = sort_names(unsorted_rows)
+
+csv_process.create_output_csv(field_names, sorted_rows)
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # read input csv file
+# with open(input_file_name, 'r') as csvfile:
+#     # creating a csv reader object
+#     csvreader = csv.reader(csvfile)
+     
+#     # extracting field names through first row
+#     fields = next(csvreader)
+ 
+#     # extracting each data row one by one
+#     for row in csvreader:
+#         rows.append(row)
+
+# #takes the second element of array
+# def take_second(elem):
+#     return elem[1] 
+
+# #sort by first name then last name
+# rows.sort()
+# rows.sort(key=take_second)
+
+# #write output csv file
+# with open(output_file_name, 'w', encoding='UTF8', newline='') as csvfile:
+#     writer = csv.writer(csvfile)
+
+#     # write the header
+#     writer.writerow(fields)
+
+#     # write the data
+#     writer.writerows(rows)
 
     
